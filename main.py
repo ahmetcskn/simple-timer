e04eade5e8cde2a5cd3d1cfb8efa56e51cc0771a
ahmetcskn <ahmet.coskun.io@hotmail.com>
2025-01-24 04:40:47 +0300

    update language

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from interface import create_interface
+
+def main():
+    root = tk.Tk()
+    root.title("Simple Timer")
+    root.geometry("200x100")
+    root.attributes("-topmost", True)
+    root.iconbitmap("")
+    create_interface(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
